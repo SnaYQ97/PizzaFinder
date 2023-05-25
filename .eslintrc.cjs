be10d7c0ac59df5abb96e67f727176f29337aec3
Artur <artowir9@wp.pl>
2023-05-25 19:30:20 +0200

    Add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,33 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/recommended',
+	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		quotes: ['error', 'single'], // Preferowane pojedyncze cudzysłowy
+		indent: ['error', 2], // Preferowane wcięcia dwoma spacjami
+		semi: ['error', 'always'], // Wymagane średniki na końcu instrukcji
+		'max-len': ['warn', { code: 130 }], // Maksymalna długość wiersza 130 znaków
+		camelcase: ['error', { properties: 'always' }], // Preferowany camelCase
+		'arrow-body-style': ['off'], // Wyłączenie wymogu jednoznacznych nawiasów klamrowych wokół bloków instrukcji
+		'no-unused-vars': ['warn', { vars: 'all', args: 'none' }], // Ostrzeżenie o nieużywanych zmiennych
+		'@typescript-eslint/explicit-module-boundary-types': 'off', // Wyłączenie wymogu określania typów zwracanych przez funkcje
+		'react/react-in-jsx-scope': 'off', // Disable the 'react/react-in-jsx-scope' rule
+		'object-curly-spacing': ['error', 'always'], // Enforce spaces between curly braces in imports
+	},
+	
 }
