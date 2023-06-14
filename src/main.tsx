1c47f844a3b7fa1131060bb93d69a4f688c86b9d
arturdomurad <artur.domurad@feildcode.com>
2023-06-14 19:09:38 +0200

    Improve Home Page and Footer Styling

--- src/main.tsx
+++ src/main.tsx
@@ -17,5 +17,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
